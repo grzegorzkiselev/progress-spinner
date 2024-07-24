c27fc26521708f851136d76f1e38d8f155c205e1
grzegorzkiselev <egorkakiselev@gmail.com>
2024-07-24 17:56:44 +0300

    feat: create pure-css iter

--- components/controls-stack/Controls-stack.js
+++
@@ -1,9 +0,0 @@
-class ControlsStack extends HTMLElement {
-  static tag = "controls-stack";
-
-  constructor() {
-    super();
-  }
-}
-
-export { ControlsStack };
--- script.js
+++
@@ -1,7 +0,0 @@
-import { InputNumber } from "./components/control/Input-number.js";
-import { ProgressSpinner } from "./components/progress-spinner/Progress-spinner.js";
-import { InputCheckbox } from "./components/control/Input-checkbox.js";
-
-customElements.define(InputCheckbox.tag, InputCheckbox);
-customElements.define(InputNumber.tag, InputNumber);
-customElements.define(ProgressSpinner.tag, ProgressSpinner);
